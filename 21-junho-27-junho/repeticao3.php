f22a96adbefbb83e1acd54c4f5a0726818a0da28
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-07-18 21:49:27 -0300

    estrutura while - exercicio 6 refazendo a tabuada

--- 21-junho-27-junho/repeticao3.php
+++ 21-junho-27-junho/repeticao3.php
@@ -3,14 +3,14 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Estrutura de repetição while: 3</title>
+	<title>Estrutura de repetição exercicio 6</title>
 	<link rel="stylesheet" href="">
 </head>
 <body>
 	<nav>
       <ul>
         <li><a href="index.php">VOLTAR A PAGINA PRINCIPAL</a></li>
-          <center><p><h1>2.35 - Fundamentos de Programação - Estrutras de Controle - Repetição - While - Parte 3</h1></p></center>  
+          <center><p><h1>2.36- Fundamentos de Programação - Estrutras de Controle - Repetição - While - Exercício 6</h1></p></center>  
       </ul>      
   	</nav>
 
