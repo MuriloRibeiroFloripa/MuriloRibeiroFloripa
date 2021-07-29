93599cb50e44b821d75bed7da6cfa0a3113712c0
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-07-28 22:53:42 -0300

    Banco de dados com PHP e MYSQL

---
+++ 26-julho-01-agosto/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>26 - junho - 01 Agosto</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<nav>
+      <ul>
+        <li><a href="..\index.php">VOLTAR A PAGINA PRINCIPAL</a></li>
+        <li><p>3.1- Banco de Dados: Introdução - Parte 1</p></li>
+        <a href="parte1.php">Aula - Parte 1</a>
+        <li><p>3.2 - Introdução - Parte 2</p></li> 
+        <a href="parte2.php">Aula - Parte 2</a>
+        <li><p>3.3 - Introdução - Parte 3</p></li> 
+           
+            
+           
+      </ul>
+  </nav>
+	
+</body>
+</html>
---
+++ 26-julho-01-agosto/parte2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Aula 2</title>
+	<link rel="stylesheet" href="">
+</head>
+<body background="imagens\parte2.png" bgproperties="fixed">
+	<nav>
+      <ul>
+        <li><a href="..\index.php">VOLTAR A PAGINA PRINCIPAL</a></li>
+      </ul>
+    </nav>	
+</body>
+</html>
